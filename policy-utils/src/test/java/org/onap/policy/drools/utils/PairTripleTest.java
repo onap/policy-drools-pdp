6b16b8f4c2c4356514fcfcd2e824c01ed014b552
Magnusen, Drew (dm741q) <dm741q@att.com>
2017-09-07 08:55:17 -0500

    Addition of State Management Feature
    Patch 1:This commit adds the feature to provide node state management.
    There are also a couple of very minor cleanup items in
    feature-session-persistence which came up during review and testing.
    Patch 2: Cleaned up some logging statements and exceptions per
    comments by Pam Dragosh. Patch 3: Clean up per comments from
    Jorge Hernandez.
    
    Issue-ID: POLICY-155
    Change-Id: I4fbfa33314d488ff46764931ca965f802b6a26d5
    Signed-off-by: Kevin McKiou <km097d@att.com>

---
+++ policy-utils/src/test/java/org/onap/policy/drools/utils/PairTripleTest.java
@@ -0,0 +1,65 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * policy-utils
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PairTripleTest {
+    
+    @Test
+    public void pairTest() {
+        Pair<String, String> p = new Pair<String, String>("foo", "bar");
+        
+       assertEquals(p.first(),"foo");
+       assertEquals(p.second(),"bar");
+       assertEquals(p.getFirst(),"foo");
+       assertEquals(p.getSecond(),"bar");
+       
+       p.first("one");
+       p.second("two");
+
+       assertEquals(p.first(),"one");
+       assertEquals(p.second(),"two");
+       assertEquals(p.getFirst(),"one");
+       assertEquals(p.getSecond(),"two");
+        
+    } 
+
+    @Test
+    public void tripleTest() {
+        Triple<String, String, String> t = new Triple<String, String,String>("foo", "bar", "fiz");
+        
+       assertEquals(t.first(),"foo");
+       assertEquals(t.second(),"bar");
+       assertEquals(t.third(),"fiz");
+       
+       t.first("one");
+       t.second("two");
+       t.third("three");
+
+       assertEquals(t.first(),"one");
+       assertEquals(t.second(),"two");
+       assertEquals(t.third(),"three");   
+    }
+
+}
