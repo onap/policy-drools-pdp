4c53995dd9917f05b9558bb81aa33caf9e8f0f97
Kevin McKiou <km097d@att.com>
2017-09-14 12:31:02 -0500

    Addition of Active-Standby Feature
    Patch 1: Adds the active-standby feature to drools-pdp.
    This feature provides the state control of the
    drools-pdp nodes controlling failover with a site
    and across sites. Patch 2: Resolve merge conflict in
    packages/install/pom.xml and pom.xml. Patch 3: Resolved
    comments from Pamela Dragosh and Jorge Hernandez.
    
    Issue-ID: POLICY-156
    Change-Id: I922b3d5d8a464006e9675924bcbc7409d68c08d5
    Signed-off-by: Kevin McKiou <km097d@att.com>

---
+++ api-active-standby-management/src/main/java/org/onap/policy/drools/activestandby/ActiveStandbyFeatureAPI.java
@@ -0,0 +1,63 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * api-active-standby-management
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.activestandby;
+
+import org.onap.policy.drools.utils.OrderedService;
+import org.onap.policy.drools.utils.OrderedServiceImpl;
+
+/**
+ * This interface provides a way to invoke optional features at various
+ * points in the code. At appropriate points in the
+ * application, the code iterates through this list, invoking these optional
+ * methods.
+ */
+public interface ActiveStandbyFeatureAPI extends OrderedService
+{
+	/**
+	 * 'FeatureAPI.impl.getList()' returns an ordered list of objects
+	 * implementing the 'FeatureAPI' interface.
+	 */
+	public static OrderedServiceImpl<ActiveStandbyFeatureAPI> impl =
+					new OrderedServiceImpl<>(ActiveStandbyFeatureAPI.class);
+
+	/**
+	 * This method returns the resourceName (PDP ID) for the Drools-PDP that is
+	 * currently designated as active.
+	 * 
+	 * @return String (resourceName)
+	 */
+	public String getPdpdNowActive();
+
+	/**
+	 * This method returns the resourceName (PDP ID) for the Drools-PDP that was
+	 * previously designated as active.
+	 * 
+	 * @return String (resourceName)
+	 */
+	public String getPdpdLastActive();
+
+	/**
+	 * This method returns the resourceName associated with this instance of the feature 
+	 * @return String (resourceName)
+	 */
+	public String getResourceName();
+
+}
