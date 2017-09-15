fa720dc93a52ed9cfaa957c9ff9ba3f140e3fd79
Magnusen, Drew (dm741q) <dm741q@att.com>
2017-09-15 14:41:44 -0500

    Added junit for policy-management
    Junits were added to cover the FileSystemPersistence
    and RestManager classes
    
    Patch 2: Merged some test functionality from FileSystemPersistenceTest
    class into SystemPersistenceTest class. Deleted FileSystemPersistenceTest
    class.
    
    Issue-ID: POLICY-109
    Change-Id: Ifd058b9988db30f6b5bc49ae54e5060c67f4ad5e
    Signed-off-by: Magnusen, Drew (dm741q) <dm741q@att.com>

--- policy-management/src/test/java/org/onap/policy/drools/persistence/test/SystemPersistenceTest.java
+++ policy-management/src/test/java/org/onap/policy/drools/persistence/test/SystemPersistenceTest.java
@@ -19,17 +19,23 @@
  */
 package org.onap.policy.drools.persistence.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.onap.policy.drools.persistence.FileSystemPersistence;
 import org.onap.policy.drools.persistence.SystemPersistence;
 import org.onap.policy.drools.properties.PolicyProperties;
 import org.slf4j.Logger;
@@ -65,6 +71,14 @@ public class SystemPersistenceTest {
    */
   public static final String TEST_CONTROLLER_FILE_BAK =
       TEST_CONTROLLER_NAME + "-controller.properties.bak";
+  
+  /**
+   * Test JUnit Environment/Engine properties
+   */
+  private static final String ENV_PROPS = "envProps";
+  private static final String ENV_PROPS_FILE = ENV_PROPS + ".environment";
+  private static final String POLICY_ENGINE_PROPERTIES_FILE = "policy-engine.properties";
+  
 
   @Test
   public void nonDefaultConfigDir() throws IOException {
@@ -78,14 +92,51 @@ public class SystemPersistenceTest {
     assertTrue(SystemPersistence.manager.getConfigurationPath().toString()
         .equals(SystemPersistence.DEFAULT_CONFIGURATION_DIR));
 
+    this.engineConfiguration();
     this.persistConfiguration();
 
     cleanUpWorkingDirs();
   }
 
+  public void engineConfiguration() {      
+    SystemPersistence.manager.setConfigurationDir(OTHER_CONFIG_DIR);
+    final Path policyEnginePropsPath = Paths.get(OTHER_CONFIG_DIR + "/" + FileSystemPersistence.PROPERTIES_FILE_ENGINE);
+    final Path environmentPropertiesPath = Paths.get(OTHER_CONFIG_DIR + "/" + ENV_PROPS_FILE);
+      
+    Properties policyEnginePropsObject, emptyProps;
+    emptyProps = new Properties();
+    
+    List<Properties> envPropertesList = new ArrayList<>();
+    envPropertesList.add(emptyProps);
+    
+    policyEnginePropsObject = new Properties();
+    policyEnginePropsObject.setProperty("foo", "bar");
+    policyEnginePropsObject.setProperty("fiz", "buz");
+   
+    try { 
+        
+        if (Files.notExists(environmentPropertiesPath)) {
+            Files.createFile(environmentPropertiesPath);
+        }
+        
+        if (Files.notExists(policyEnginePropsPath)) {
+            OutputStream fout = new FileOutputStream(policyEnginePropsPath.toFile());
+            policyEnginePropsObject.store(fout, "");
+            fout.close();
+        } 
+    } catch (IOException e) {
+            logger.error("Problem creating {}", policyEnginePropsPath);
+    }
+    
+    assertEquals(SystemPersistence.manager.getEngineProperties(), policyEnginePropsObject);
+    assertEquals(SystemPersistence.manager.getEnvironmentProperties(ENV_PROPS), emptyProps);
+    assertEquals(SystemPersistence.manager.getEnvironmentProperties(), envPropertesList);
+    
+  }
+  
   public void persistConfiguration() {
     logger.info("enter");
-
+  
     final Path controllerPath = Paths
         .get(SystemPersistence.manager.getConfigurationPath().toString(), TEST_CONTROLLER_FILE);
 
@@ -121,9 +172,18 @@ public class SystemPersistenceTest {
     final Path testControllerBakPath = Paths
         .get(SystemPersistence.manager.getConfigurationPath().toString(), TEST_CONTROLLER_FILE_BAK);
 
+    final Path policyEnginePath = Paths
+        .get(OTHER_CONFIG_DIR + "/" + POLICY_ENGINE_PROPERTIES_FILE);
+    
+    final Path environmentPath = Paths
+        .get(OTHER_CONFIG_DIR + "/" + ENV_PROPS_FILE);
+    
     Files.deleteIfExists(testControllerPath);
     Files.deleteIfExists(testControllerBakPath);
+    Files.deleteIfExists(policyEnginePath);
+    Files.deleteIfExists(environmentPath);
     Files.deleteIfExists(Paths.get(OTHER_CONFIG_DIR));
+    
   }
 
 }
