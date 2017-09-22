c8c80622a513d02d59ffbd013c88a1b36c6bda4d
Kevin McKiou <km097d@att.com>
2017-09-22 17:08:15 -0500

    Incr State Mgmt Code Coverage
    Patch 1: Added JUnit tests to feature-state-management
    to increase coverage. Estimated coverage is now 48%.
    Patch 2: Trivial change to force a rebuild. Patch 3:
    Tweaking the JUnit to try and account for the difference
    between the LF environment and my local environment.
    Patch 4: Something caused the policy endpoints JUnits
    to fail - unrelated to these changes.  Made a trival
    change to force a rebuild.
    
    Issue-ID; POLICY-266
    Change-Id: I7979c200ab18d5861ba20e0d5f23bd0083193daa
    Signed-off-by: Kevin McKiou <km097d@att.com>

---
+++ feature-state-management/src/test/java/org/onap/policy/drools/statemanagement/test/Audit.java
@@ -0,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Integrity Monitor
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.statemanagement.test;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Audit")
+
+public class Audit implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="name", length=64, unique=true)
+	private String name;
+	
+	public Audit() {
+		//default constructor
+	}
+}
