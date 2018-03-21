1f144c53eb1d0a5aa8a0f900ed4796d60e9d6804
Magnusen, Drew (dm741q) <dm741q@att.com>
2018-03-21 16:44:45 -0500

    Implementation of distributed locking feature
    Beginning of locking implementation
    Patch 2: More logic added to grabLock and checkLock methods
    Patch 3: Cleaning up TargetLock a bit/refactoring
    Patch 4: Implementation of unlock/deleteLock methods. Code cleanup
    Patch 5: Added assembly folder and assemble_zip.xml
    Patch 6: removed TargetLockTest, will resubmit later with in memorydb
    Patch 7: changed while loop in lock() to a do-while loop
    Patch 8: Simplified loop in lock method, replaced all instances of
    printStrackTrace will logger statements, added check in lock() loop to
    ensure we have not exceeded our expiration time instead of waiting
    indefinitely for a lock, simplification of return statements.
    Patch 9: Moved call to cleanUp before Thread.interrupt call. Cleaner
    comments.
    
    Issue-ID: POLICY-699
    Change-Id: I012fd37926ccbbdd87a3e4acb2788b53680115f0
    Signed-off-by: Magnusen, Drew (dm741q) <dm741q@att.com>

---
+++ feature-distributed-locking/src/main/java/org/onap/policy/distributed/locking/DistributedLockingFeature.java
@@ -0,0 +1,70 @@
+/*
+ * ============LICENSE_START=======================================================
+ * feature-distributed-locking
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.distributed.locking;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.onap.policy.drools.core.PolicySessionFeatureAPI;
+import org.onap.policy.drools.features.PolicyEngineFeatureAPI;
+import org.onap.policy.drools.utils.PropertyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DistributedLockingFeature implements PolicyEngineFeatureAPI, PolicySessionFeatureAPI{
+	
+	// get an instance of logger 
+	private static final Logger  logger = LoggerFactory.getLogger(DistributedLockingFeature.class);
+	private Properties lockProps;
+	
+	@Override
+	public int getSequenceNumber() {
+        return 1000;
+	}
+
+	public boolean lockResource(String resourceId, String owner) {
+		TargetLock tLock = new TargetLock(resourceId, owner, System.currentTimeMillis() + 5000, lockProps);
+		
+		return tLock.lock();
+			
+	}
+	
+	public boolean unlockResource(String resourceId, String owner) {
+		TargetLock tLock = new TargetLock(resourceId, owner, System.currentTimeMillis() + 5000, lockProps);
+		
+		return tLock.unlock();
+		
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void globalInit(String[] args, String configDir) {
+
+		try {
+			lockProps = PropertyUtil.getProperties(configDir + "/feature-distributed-locking.properties");
+
+		} catch (IOException e1) {
+			logger.error("initializing DistributedLockingFeature: ", e1);
+		}
+
+	}
+}
