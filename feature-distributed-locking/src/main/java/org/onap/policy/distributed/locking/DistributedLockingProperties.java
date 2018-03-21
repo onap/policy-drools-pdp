ef2dc2dc5121534afdb475489e701be0b256f076
Magnusen, Drew (dm741q) <dm741q@att.com>
2018-03-21 16:44:45 -0500

    Implementation of distributed locking feature
    Beginning of locking implementation
    
    Issue-ID: POLICY-699
    Change-Id: I012fd37926ccbbdd87a3e4acb2788b53680115f0
    Signed-off-by: Magnusen, Drew (dm741q) <dm741q@att.com>

---
+++ feature-distributed-locking/src/main/java/org/onap/policy/distributed/locking/DistributedLockingProperties.java
@@ -0,0 +1,57 @@
+/*
+ * ============LICENSE_START=======================================================
+ * feature-active-standby-management
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.policy.distributed.locking;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class DistributedLockingProperties {
+	
+	private static final Logger  logger = LoggerFactory.getLogger(DistributedLockingProperties.class);
+	
+	public static final String DB_DRIVER = "javax.persistence.jdbc.driver";
+	public static final String DB_URL = "javax.persistence.jdbc.url";
+	public static final String DB_USER = "javax.persistence.jdbc.user";
+	public static final String DB_PWD = "javax.persistence.jdbc.password";
+
+	
+	private static Properties properties = null;
+
+	private DistributedLockingProperties(){
+	}
+
+	public static void initProperties (Properties prop){
+		logger.info("DistributedLockingProperties.initProperties(Properties): entry");
+		logger.info("\n\nDistributedLockingProperties.initProperties: Properties = \n{}\n\n", prop);
+		
+		properties = prop;
+	}
+
+	public static String getProperty(String key){
+		return properties.getProperty(key);
+	}
+	
+	public static Properties getProperties() {
+		return properties;
+	}
+}
