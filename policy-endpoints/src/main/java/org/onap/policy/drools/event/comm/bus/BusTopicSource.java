163a7a236ed8b0eb540ef3d38dcbea4f2d3f7456
Jim Hahn <jrh3@att.com>
2018-04-18 11:04:11 -0400

    Generate default consumer instance via UUID
    Modified the code to generate a consumer instance via UUID,
    when no consumer instance is provided.
    Deprecated the DEFAULT_CONSUMER_INSTANCE.
    
    Change-Id: Ia577921c613f7beea41fc8a10f4adea60b4be797
    Issue-ID: POLICY-749
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- policy-endpoints/src/main/java/org/onap/policy/drools/event/comm/bus/BusTopicSource.java
+++ policy-endpoints/src/main/java/org/onap/policy/drools/event/comm/bus/BusTopicSource.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * policy-endpoints
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ public interface BusTopicSource extends ApiKeyEnabled, TopicSource {
 	/**
 	 * Default Consumer Instance Value
 	 */
+    @Deprecated
 	public static String DEFAULT_CONSUMER_INSTANCE = "0";
 	
 	/**
