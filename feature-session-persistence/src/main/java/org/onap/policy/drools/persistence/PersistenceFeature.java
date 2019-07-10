979444200b948d3d50aa9f4b38c6079eb6de1860
Jim Hahn <jrh3@att.com>
2019-07-10 16:39:28 -0400

    Fix checkstyle issues in policy-core
    Also deleted the checkstyle suppression file.
    
    Change-Id: I93e9f9154b614b56f7826ec978e1a99bd9369348
    Issue-ID: POLICY-1907
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- feature-session-persistence/src/main/java/org/onap/policy/drools/persistence/PersistenceFeature.java
+++ feature-session-persistence/src/main/java/org/onap/policy/drools/persistence/PersistenceFeature.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * feature-session-persistence
  * ================================================================================
- * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.KieSessionConfiguration;
 import org.onap.policy.drools.core.PolicyContainer;
 import org.onap.policy.drools.core.PolicySession;
-import org.onap.policy.drools.core.PolicySessionFeatureAPI;
+import org.onap.policy.drools.core.PolicySessionFeatureApi;
 import org.onap.policy.drools.features.PolicyEngineFeatureAPI;
 import org.onap.policy.drools.system.PolicyController;
 import org.onap.policy.drools.system.PolicyEngine;
@@ -62,7 +62,7 @@ import org.slf4j.LoggerFactory;
  * <p>The bulk of the code here was once in other classes, such as 'PolicyContainer' and 'Main'. It
  * was moved here as part of making this a separate optional feature.
  */
-public class PersistenceFeature implements PolicySessionFeatureAPI, PolicyEngineFeatureAPI {
+public class PersistenceFeature implements PolicySessionFeatureApi, PolicyEngineFeatureAPI {
 
     private static final Logger logger = LoggerFactory.getLogger(PersistenceFeature.class);
     
