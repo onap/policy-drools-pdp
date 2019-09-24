1ab235ec5fadbedc91c8767f214395ca7af9059c
Jim Hahn <jrh3@att.com>
2019-09-24 10:51:21 -0400

    Reimplement Lock API using Lock objects
    Change-Id: Iddba38157ddc5f7277656979c0e679e5489eb7b1
    Issue-ID: POLICY-2113
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- policy-core/src/main/java/org/onap/policy/drools/core/lock/PolicyResourceLockManager.java
+++ policy-core/src/main/java/org/onap/policy/drools/core/lock/PolicyResourceLockManager.java
@@ -21,198 +21,115 @@
 package org.onap.policy.drools.core.lock;
 
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import org.onap.policy.drools.core.lock.PolicyResourceLockFeatureApi.OperResult;
+import java.util.concurrent.atomic.AtomicReference;
+import org.onap.policy.common.utils.services.OrderedServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Manager of resource locks. Checks for API implementers.
  */
-public class PolicyResourceLockManager extends SimpleLockManager {
-
+public class PolicyResourceLockManager {
     private static Logger logger = LoggerFactory.getLogger(PolicyResourceLockManager.class);
 
     /**
-     * Used by junit tests.
+     * 'FeatureAPI.impl.getList()' returns an ordered list of objects implementing the
+     * 'FeatureAPI' interface.
      */
-    protected PolicyResourceLockManager() {
+    private static final OrderedServiceImpl<PolicyResourceLockFeatureApi> providers =
+                    new OrderedServiceImpl<>(PolicyResourceLockFeatureApi.class);
+
+    /**
+     * Feature implementation that was discovered.
+     */
+    private AtomicReference<PolicyResourceLockFeatureApi> factory = new AtomicReference<>();
+
+
+    /**
+     * Constructs the object.
+     */
+    public PolicyResourceLockManager() {
         super();
     }
 
     /**
-     * Get instance.
+     * Gets the lock manager singleton.
      *
-     * @return the manager singleton
+     * @return the lock manager
      */
     public static PolicyResourceLockManager getInstance() {
         return Singleton.instance;
     }
 
-    @Override
-    public boolean lock(String resourceId, String owner, int holdSec) {
-        if (resourceId == null) {
-            throw makeNullArgException(MSG_NULL_RESOURCE_ID);
-        }
-
-        if (owner == null) {
-            throw makeNullArgException(MSG_NULL_OWNER);
-        }
-
-
-        return doBoolIntercept(impl -> impl.beforeLock(resourceId, owner, holdSec), () -> {
-
-            // implementer didn't do the work - defer to the superclass
-            boolean locked = super.lock(resourceId, owner, holdSec);
-
-            doIntercept(false, impl -> impl.afterLock(resourceId, owner, locked));
-
-            return locked;
-        });
-    }
-
-    @Override
-    public boolean refresh(String resourceId, String owner, int holdSec) {
-        if (resourceId == null) {
-            throw makeNullArgException(MSG_NULL_RESOURCE_ID);
-        }
-
-        if (owner == null) {
-            throw makeNullArgException(MSG_NULL_OWNER);
-        }
-
-
-        return doBoolIntercept(impl -> impl.beforeRefresh(resourceId, owner, holdSec), () -> {
-
-            // implementer didn't do the work - defer to the superclass
-            boolean refreshed = super.refresh(resourceId, owner, holdSec);
-
-            doIntercept(false, impl -> impl.afterRefresh(resourceId, owner, refreshed));
-
-            return refreshed;
-        });
-    }
-
-    @Override
-    public boolean unlock(String resourceId, String owner) {
-        if (resourceId == null) {
-            throw makeNullArgException(MSG_NULL_RESOURCE_ID);
-        }
-
-        if (owner == null) {
-            throw makeNullArgException(MSG_NULL_OWNER);
-        }
-
-
-        return doBoolIntercept(impl -> impl.beforeUnlock(resourceId, owner), () -> {
-
-            // implementer didn't do the work - defer to the superclass
-            boolean unlocked = super.unlock(resourceId, owner);
-
-            doIntercept(false, impl -> impl.afterUnlock(resourceId, owner, unlocked));
-
-            return unlocked;
-        });
-    }
-
     /**
-     * Is locked.
+     * Requests a lock on a resource. Typically, the lock is not immediately granted,
+     * though a "lock" object is always returned. Once the lock has been granted (or
+     * denied), the callback will be invoked to indicate the result.
      *
-     * @throws IllegalArgumentException if the resourceId is {@code null}
-     */
-    @Override
-    public boolean isLocked(String resourceId) {
-        if (resourceId == null) {
-            throw makeNullArgException(MSG_NULL_RESOURCE_ID);
-        }
-
-
-        return doBoolIntercept(impl -> impl.beforeIsLocked(resourceId), () ->
-
-           // implementer didn't do the work - defer to the superclass
-           super.isLocked(resourceId)
-        );
-    }
-
-    /**
-     *  Is locked by.
+     * <p/>
+     * Note: the callback may be invoked before this method returns.
      *
-     * @throws IllegalArgumentException if the resourceId or owner is {@code null}
-     */
-    @Override
-    public boolean isLockedBy(String resourceId, String owner) {
-        if (resourceId == null) {
-            throw makeNullArgException(MSG_NULL_RESOURCE_ID);
-        }
-
-        if (owner == null) {
-            throw makeNullArgException(MSG_NULL_OWNER);
-        }
-
-        return doBoolIntercept(impl -> impl.beforeIsLockedBy(resourceId, owner), () ->
-
-            // implementer didn't do the work - defer to the superclass
-            super.isLockedBy(resourceId, owner)
-        );
-    }
-
-    /**
-     * Applies a function to each implementer of the lock feature. Returns as soon as one
-     * of them returns a result other than <b>OPER_UNHANDLED</b>. If they all return
-     * <b>OPER_UNHANDLED</b>, then it returns the result of applying the default function.
+     * <p/>
+     * If an implementation throws an exception, or if no implementation is available,
+     * then the {@link LockCallback#lockUnavailable(Lock)} method is called and a
+     * {@link Lock} is returned that never succeeds.
      *
-     * @param interceptFunc intercept function
-     * @param defaultFunc default function
-     * @return {@code true} if success, {@code false} otherwise
+     * @param resourceId identifier of the resource to be locked
+     * @param ownerInfo information identifying the owner requesting the lock
+     * @param holdSec amount of time, in seconds, for which the lock should be held once
+     *        it has been granted, after which it will automatically be released
+     * @param callback callback to be invoked once the lock is granted, or subsequently
+     *        lost; must not be {@code null}
+     * @param waitForLock {@code true} to wait for the lock, if it is currently locked,
+     *        {@code false} otherwise
+     * @return a new lock
      */
-    private boolean doBoolIntercept(Function<PolicyResourceLockFeatureApi, OperResult> interceptFunc,
-                    Supplier<Boolean> defaultFunc) {
+    public Lock lock(String resourceId, Object ownerInfo, int holdSec, LockCallback callback, boolean waitForLock) {
 
-        OperResult result = doIntercept(OperResult.OPER_UNHANDLED, interceptFunc);
-        if (result != OperResult.OPER_UNHANDLED) {
-            return (result == OperResult.OPER_ACCEPTED);
-        }
+        String featureName = "unknown";
 
-        return defaultFunc.get();
-    }
+        try {
+            // first try the cached factory
+            PolicyResourceLockFeatureApi feature = factory.get();
+            if (feature != null) {
+                featureName = feature.getClass().getName();
+                return feature.lock(resourceId, ownerInfo, holdSec, callback, waitForLock);
+            }
 
-    /**
-     * Applies a function to each implementer of the lock feature. Returns as soon as one
-     * of them returns a non-null value.
-     *
-     * @param continueValue if the implementer returns this value, then it continues to
-     *        check addition implementers
-     * @param func function to be applied to the implementers
-     * @return first non-null value returned by an implementer, <i>continueValue</i> if
-     *       they all returned <i>continueValue</i>
-     */
-    private <T> T doIntercept(T continueValue, Function<PolicyResourceLockFeatureApi, T> func) {
-
-        for (PolicyResourceLockFeatureApi impl : getImplementers()) {
-            try {
-                T result = func.apply(impl);
-                if (result != continueValue) {
-                    return result;
+            // nothing cached - find an implementer
+            for (PolicyResourceLockFeatureApi impl : getProviders()) {
+                featureName = impl.getClass().getName();
+                if (!impl.enabled()) {
+                    continue;
                 }
 
-            } catch (RuntimeException e) {
-                logger.warn("lock feature {} threw an exception", impl, e);
+                logger.info("{}: choosing feature {}", this, featureName);
+
+                factory.compareAndSet(null, impl);
+
+                feature = factory.get();
+                featureName = feature.getClass().getName();
+                return feature.lock(resourceId, ownerInfo, holdSec, callback, waitForLock);
             }
+
+        } catch (RuntimeException e) {
+            logger.error("{}: feature {} lock() failure because of {}", this, featureName, e.getMessage(), e);
         }
 
-        return continueValue;
+
+        // no implementer available at this time - return a lock that always fails
+
+        logger.warn("{}: no locking features available at this time", this);
+        LockImpl lock = new LockImpl(LockState.UNAVAILABLE, resourceId, ownerInfo, null, holdSec, callback, false);
+        lock.notifyUnavailable();
+
+        return lock;
     }
 
     // these may be overridden by junit tests
 
-    /**
-     * Get implementers.
-     *
-     * @return the list of feature implementers
-     */
-    protected List<PolicyResourceLockFeatureApi> getImplementers() {
-        return PolicyResourceLockFeatureApiConstants.getImpl().getList();
+    protected List<PolicyResourceLockFeatureApi> getProviders() {
+        return providers.getList();
     }
 
     /**
---
+++ policy-core/src/test/java/org/onap/policy/drools/core/lock/LockImplTest.java
@@ -0,0 +1,195 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.core.lock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LockImplTest {
+    private static final LockState STATE = LockState.WAITING;
+    private static final LockState STATE2 = LockState.ACTIVE;
+    private static final String RESOURCE = "hello";
+    private static final Object OWNER = new Object();
+    private static final String OWNER_KEY = "world";
+    private static final int HOLD_SEC = 10;
+    private static final int HOLD_SEC2 = 20;
+    private static final String EXPECTED_EXCEPTION = "expected exception";
+
+    private LockCallback callback;
+    private LockCallback callback2;
+    private LockImpl lock;
+
+    /**
+     * Populates {@link #lock}.
+     */
+    @Before
+    public void setUp() {
+        callback = mock(LockCallback.class);
+        callback2 = mock(LockCallback.class);
+
+        lock = new LockImpl(STATE, RESOURCE, OWNER, OWNER_KEY, HOLD_SEC, callback, false);
+    }
+
+    @Test
+    public void testLockImpl_testGetters() {
+        assertEquals(STATE, lock.getState());
+        assertEquals(RESOURCE, lock.getResourceId());
+        assertEquals(OWNER, lock.getOwnerInfo());
+        assertEquals(OWNER_KEY, lock.getOwnerKey());
+        assertSame(callback, lock.getCallback());
+        assertEquals(HOLD_SEC, lock.getHoldSec());
+    }
+
+    @Test
+    public void testFree() {
+        assertFalse(lock.free());
+
+        verify(callback, never()).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+    }
+
+    @Test
+    public void testFree_Succeed() {
+        lock = new LockImpl(LockState.ACTIVE, RESOURCE, OWNER, OWNER_KEY, HOLD_SEC, callback, true);
+
+        assertTrue(lock.free());
+        assertFalse(lock.isActive());
+
+        // should now return false
+        assertFalse(lock.free());
+        assertFalse(lock.isActive());
+
+        verify(callback, never()).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+    }
+
+    @Test
+    public void testExtend() {
+        // succeed is set to "false", thus extend should fail
+        lock.extend(HOLD_SEC, callback2);
+
+        verify(callback, never()).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+
+        verify(callback2, never()).lockAvailable(any());
+        verify(callback2).lockUnavailable(any());
+    }
+
+    @Test
+    public void testExtend_Succeed() {
+        lock = new LockImpl(LockState.ACTIVE, RESOURCE, OWNER, OWNER_KEY, HOLD_SEC, callback, true);
+
+        lock.extend(HOLD_SEC, callback2);
+        assertTrue(lock.isActive());
+
+        verify(callback, never()).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+
+        verify(callback2).lockAvailable(any());
+        verify(callback2, never()).lockUnavailable(any());
+
+
+        // free it and then try to extend again
+        lock.free();
+
+        lock.extend(HOLD_SEC, callback2);
+        assertFalse(lock.isActive());
+
+        // should not have had any more calls
+        verify(callback, never()).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+
+        verify(callback2, times(1)).lockAvailable(any());
+
+        // should have indicated that it's unavailable
+        verify(callback2).lockUnavailable(any());
+    }
+
+    @Test
+    public void testNotifyAvailable() {
+        lock.notifyAvailable();
+
+        verify(callback).lockAvailable(any());
+        verify(callback, never()).lockUnavailable(any());
+    }
+
+    @Test
+    public void testNotifyAvailable_Ex() {
+        doThrow(new IllegalArgumentException(EXPECTED_EXCEPTION)).when(callback).lockAvailable(any());
+        doThrow(new IllegalArgumentException(EXPECTED_EXCEPTION)).when(callback).lockUnavailable(any());
+
+        // should not throw an exception
+        lock.notifyAvailable();
+    }
+
+    @Test
+    public void testNotifyUnavailable() {
+        lock.notifyUnavailable();
+
+        verify(callback, never()).lockAvailable(any());
+        verify(callback).lockUnavailable(any());
+    }
+
+    @Test
+    public void testNotifyUnavailable_Ex() {
+        doThrow(new IllegalArgumentException(EXPECTED_EXCEPTION)).when(callback).lockAvailable(any());
+        doThrow(new IllegalArgumentException(EXPECTED_EXCEPTION)).when(callback).lockUnavailable(any());
+
+        // should not throw an exception
+        lock.notifyUnavailable();
+    }
+
+    @Test
+    public void testSetState() {
+        lock.setState(STATE2);
+
+        assertEquals(STATE2, lock.getState());
+    }
+
+    @Test
+    public void testSetHoldSec() {
+        lock.setHoldSec(HOLD_SEC2);
+
+        assertEquals(HOLD_SEC2, lock.getHoldSec());
+    }
+
+    @Test
+    public void testToString() {
+        String text = lock.toString();
+
+        assertNotNull(text);
+        assertThat(text).doesNotContain("ownerInfo").doesNotContain("callback").doesNotContain("succeed");
+    }
+
+}
