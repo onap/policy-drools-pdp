daefefcbaafd7ad67514f125a468a168d106169b
Jim Hahn <jrh3@att.com>
2019-09-24 10:51:21 -0400

    Reimplement Lock API using Lock objects
    Modified PolicyResourceLockManager to just return a feature, deferring
    the lock() call/method to the feature, itself.  The manager was also
    modified so that, if it can't find an enabled provider, it will return
    a default provider, whose lock() methods always fail.  Once a feature
    has been identified, the manager will cache it for use thereafter.
    
    Modified the feature API to return lock objects and simplified the
    interface to remove the beforeXxx and afterXxx methods.
    Moved the unlock and refresh methods from the feature API into the
    lock class, renaming them to free and extend, respectively.
    Added a separate, feature-simple-locking project, which implements
    a simple version of the locking feature, over a single JVM.
    Extensively revised the distributed locking feature to fit in with
    the new API.
    
    Added support for persistence so that the various LockImpl classes
    can be serialized and still function correctly when they are
    deserialized back into new feature instances
    Added default implementations of free & extend to LockImpl.
    Modified API to take the ownerKey string, instead of the owner object.
    Removed Extractor as unneeded - may add via another review, if still useful.
    
    Updates per review comments:
    - Updated licenses in feature-simple-locking
    - Added beforeCreateLock & afterCreateLock to feature API
    - Moved SimpleLockingFeature into policy-management so that it's
      always available
    - Moved the executor service, "exsvc", into PolicyEngine
    - Moved Extrator into policy-utils
    - Changed Extractor logging level for exceptions
    - Fixed feature sequence numbers
    - Fixed mixing of seconds and milliseconds
    - Renamed exsvc
    - Modified to use property method with default value
    - Configured scheduled executor
    - Added suffix to Extractor.register()
    - Eliminated Feature Api and tied lock manager into engine
    
    Change-Id: Iddba38157ddc5f7277656979c0e679e5489eb7b1
    Issue-ID: POLICY-2113
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ feature-distributed-locking/src/main/java/org/onap/policy/distributed/locking/DistributedLockProperties.java
@@ -0,0 +1,136 @@
+/*
+ * ============LICENSE_START=======================================================
+ * feature-distributed-locking
+ * ================================================================================
+ * Copyright (C) 2018-2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distributed.locking;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.onap.policy.common.utils.properties.BeanConfigurator;
+import org.onap.policy.common.utils.properties.Property;
+import org.onap.policy.common.utils.properties.exception.PropertyException;
+
+
+@Getter
+@Setter
+public class DistributedLockProperties {
+    public static final String PREFIX = "distributed.locking.";
+
+    public static final String DB_DRIVER = "javax.persistence.jdbc.driver";
+    public static final String DB_URL = "javax.persistence.jdbc.url";
+    public static final String DB_USER = "javax.persistence.jdbc.user";
+    public static final String DB_PASS = "javax.persistence.jdbc.password";
+    public static final String TRANSIENT_ERROR_CODES = PREFIX + "transient.error.codes";
+    public static final String EXPIRE_CHECK_SEC = PREFIX + "expire.check.seconds";
+    public static final String RETRY_SEC = PREFIX + "retry.seconds";
+    public static final String MAX_RETRIES = PREFIX + "max.retries";
+
+    /**
+     * Database driver.
+     */
+    @Property(name = DB_DRIVER)
+    private String dbDriver;
+
+    /**
+     * Database url.
+     */
+    @Property(name = DB_URL)
+    private String dbUrl;
+
+    /**
+     * Database user.
+     */
+    @Property(name = DB_USER)
+    private String dbUser;
+
+    /**
+     * Database password.
+     */
+    @Property(name = DB_PASS)
+    private String dbPwd;
+
+    /**
+     * Vendor-specific error codes that are "transient", meaning they may go away if the
+     * command is repeated (e.g., connection issue), as opposed to something like a syntax
+     * error or a duplicate key.
+     */
+    @Property(name = TRANSIENT_ERROR_CODES)
+    private String errorCodeStrings;
+
+    private final Set<Integer> transientErrorCodes;
+
+    /**
+     * Time, in seconds, to wait between checks for expired locks.
+     */
+    @Property(name = EXPIRE_CHECK_SEC, defaultValue = "900")
+    private int expireCheckSec;
+
+    /**
+     * Number of seconds to wait before retrying, after a DB error.
+     */
+    @Property(name = RETRY_SEC, defaultValue = "60")
+    private int retrySec;
+
+    /**
+     * Maximum number of times to retry a DB operation.
+     */
+    @Property(name = MAX_RETRIES, defaultValue = "2")
+    private int maxRetries;
+
+    /**
+     * Constructs the object, populating fields from the properties.
+     *
+     * @param props properties from which to configure this
+     * @throws PropertyException if an error occurs
+     */
+    public DistributedLockProperties(Properties props) throws PropertyException {
+        new BeanConfigurator().configureFromProperties(this, props);
+
+        Set<Integer> set = new HashSet<>();
+        for (String text : errorCodeStrings.split(",")) {
+            text = text.trim();
+            if (text.isEmpty()) {
+                continue;
+            }
+
+            try {
+                set.add(Integer.valueOf(text));
+
+            } catch (NumberFormatException e) {
+                throw new PropertyException(TRANSIENT_ERROR_CODES, "errorCodeStrings", e);
+            }
+        }
+
+        transientErrorCodes = Collections.unmodifiableSet(set);
+    }
+
+    /**
+     * Determines if an error is transient.
+     *
+     * @param errorCode error code to check
+     * @return {@code true} if the error is transient, {@code false} otherwise
+     */
+    public boolean isTransient(int errorCode) {
+        return transientErrorCodes.contains(errorCode);
+    }
+}
--- policy-core/src/main/java/org/onap/policy/drools/core/lock/LockState.java
+++ policy-core/src/main/java/org/onap/policy/drools/core/lock/LockState.java
@@ -1,15 +1,15 @@
-/*
+/*-
  * ============LICENSE_START=======================================================
- * feature-distributed-locking
+ * ONAP
  * ================================================================================
- * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,17 +18,26 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.policy.distributed.locking;
+package org.onap.policy.drools.core.lock;
 
-public class DistributedLockingFeatureException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
+/**
+ * States of a Lock.
+ */
+
+public enum LockState {
 
     /**
-     * Constructor.
-     * 
-     * @param ex exception to be wrapped
+     * Waiting for the lock request to complete.
      */
-    public DistributedLockingFeatureException(Exception ex) {
-        super(ex);
-    }
+    WAITING,
+
+    /**
+     * This lock currently holds the resource.
+     */
+    ACTIVE,
+
+    /**
+     * The resource is no longer available to the lock.
+     */
+    UNAVAILABLE
 }
