f9e71ded2e39d6a21952bc1670ec4947d8c47d00
Straubs, Ralph (rs8887) <rs8887@att.com>
2019-10-28 07:49:56 -0500

    Provide a way to trigger Drools rules when an update occurs
    An instance of 'DroolsInitFeature.Init' is inserted into a Drools
    session when it is initialized, updated, or restored from
    persistent data. This provides an opportunity to catch more
    initialization scenarios than you can with an empty 'when' clause.
    
    Updates:
    
    1) Change 'DroolsInitFeature' to use Policy Engine scheduled executor
    
    2) Add new test to 'DroolsInitFeatureTest' to verify that the 'Init'
       object is inserted into and deleted from Drools memory
    
    3) Remove trailing spaces
    
    Issue-ID: POLICY-1948
    Change-Id: I4748c2415c4563e98b4a53d573c9ebe1e96e8e34
    Signed-off-by: Straubs, Ralph (rs8887) <rs8887@att.com>

---
+++ feature-drools-init/src/main/java/org/onap/policy/drools/droolsinit/DroolsInitFeature.java
@@ -0,0 +1,87 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * feature-drools-init
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.droolsinit;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+import org.kie.api.runtime.rule.FactHandle;
+
+import org.onap.policy.drools.core.PolicySession;
+import org.onap.policy.drools.core.PolicySessionFeatureApi;
+import org.onap.policy.drools.system.PolicyEngineConstants;
+
+/**
+ * This feature inserts an object of class 'DroolsInitFeature.Init' into
+ * every newly-created or updated Drools session, including those that were
+ * initialized with persistent data. Rules matching on objects of this type
+ * can then do things like initialize global data.
+ */
+public class DroolsInitFeature implements PolicySessionFeatureApi {
+    // default delay is 10 minutes
+    private static final long DELAY = 600000L;
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public int getSequenceNumber() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public PolicySession.ThreadModel selectThreadModel(PolicySession policySession) {
+        new Init(policySession);
+        return null;
+    }
+
+    /**
+     * Instances of this class are inserted into Drools memory.
+     */
+    public static class Init implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Place this instance in Drools memory, and then remove it after
+         * one minute, if it is still there.
+         *
+         * @param policySession the associated session
+         */
+        public Init(final PolicySession policySession) {
+            // insert this instance into Drools memory
+            final FactHandle factHandle = policySession.getKieSession().insert(this);
+
+            // after 10 minutes, remove the object from Drools memory (if needed)
+            PolicyEngineConstants.getManager().getExecutorService().schedule(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        if (policySession.getKieSession().getObject(factHandle) != null) {
+                            // object has not been removed by application -- remove it here
+                            policySession.getKieSession().delete(factHandle);
+                        }
+                    }
+                }, DELAY, TimeUnit.MILLISECONDS);
+        }
+    }
+}
