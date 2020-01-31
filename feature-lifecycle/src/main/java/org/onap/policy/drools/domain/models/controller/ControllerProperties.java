baa4e928207f5930fcd620bddddcd493aa7a4122
jhh <jorge.hernandez-herrero@att.com>
2020-01-30 18:57:13 -0600

    domain native and operational policies support
    - moved DomainMaker to policy-utils/ project.
    - support for native drools and controller drools policy types.
    
    Issue-ID: POLICY-2345
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: I46b9816eadceb7ef280e0e6789f07b4d4ca51519
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

---
+++ feature-lifecycle/src/main/java/org/onap/policy/drools/domain/models/controller/ControllerProperties.java
@@ -0,0 +1,52 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.domain.models.controller;
+
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+
+/**
+ * Controller Domain Policy.
+ */
+
+@Data
+@SuperBuilder
+public class ControllerProperties implements Serializable {
+    private static final long serialVersionUID = 1259434187110418986L;
+
+    @SerializedName("controllerName")
+    protected String controllerName;
+
+    @SerializedName("sourceTopics")
+    protected List<ControllerSourceTopic> sourceTopics;
+
+    @SerializedName("sinkTopics")
+    protected List<ControllerSinkTopic> sinkTopics;
+
+    @SerializedName("customConfig")
+    protected Map<String, String> customConfig;
+
+}
---
+++ feature-lifecycle/src/test/java/org/onap/policy/drools/domain/models/DroolsPolicyModelsTest.java
@@ -0,0 +1,86 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.domain.models;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.filters.FilterChain;
+import com.openpojo.reflection.filters.FilterClassName;
+import com.openpojo.reflection.filters.FilterNonConcrete;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.onap.policy.drools.domain.models.controller.ControllerPolicy;
+import org.onap.policy.drools.domain.models.controller.ControllerProperties;
+import org.onap.policy.drools.domain.models.nativ.rules.NativeDroolsController;
+import org.onap.policy.drools.domain.models.nativ.rules.NativeDroolsPolicy;
+import org.onap.policy.drools.domain.models.nativ.rules.NativeDroolsProperties;
+import org.onap.policy.drools.domain.models.nativ.rules.NativeDroolsRulesArtifact;
+
+public class DroolsPolicyModelsTest {
+
+    @Test
+    public void testPackage() {
+        /* validate model pojos */
+        List<PojoClass> pojoClasses =
+                PojoClassFactory
+                        .getPojoClassesRecursively("org.onap.policy.drools.domain.models",
+                            new FilterChain(new FilterNonConcrete(),
+                                    new FilterClassName(DroolsPolicy.class.getName())));
+
+        Validator validator = ValidatorBuilder.create()
+                                      .with(new SetterTester(), new GetterTester()).build();
+        validator.validate(pojoClasses);
+    }
+
+    @Test
+    public void testBuildDomainPolicyNativeDrools() {
+        /* manually create a native drools policy */
+        NativeDroolsPolicy.builder().metadata(Metadata.builder().policyId("policy-id").build()).name("example")
+            .type("onap.policies.native.Drools")
+            .typeVersion("1.0.0")
+            .version("1.0.0")
+            .properties(
+                NativeDroolsProperties.builder().controller(
+                        NativeDroolsController.builder().name("example").version("1.0.0").build())
+                        .rulesArtifact(
+                                NativeDroolsRulesArtifact.builder().groupId("org.onap.policy.controlloop")
+                                        .artifactId("example").version("example").build()).build())
+            .build();
+    }
+
+    @Test
+    public void testBuildDomainPolicyController() {
+        /* manually create a controller policy */
+        ControllerPolicy.builder().metadata(Metadata.builder().policyId("policy-id").build()).name("example")
+            .version("1.0.0")
+            .type("onap.policies.drools.Controller")
+            .typeVersion("1.0.0")
+            .properties(ControllerProperties.builder().controllerName("example").sourceTopics(
+                    new ArrayList<>()).sinkTopics(new ArrayList<>()).build())
+            .build();
+    }
+
+}
