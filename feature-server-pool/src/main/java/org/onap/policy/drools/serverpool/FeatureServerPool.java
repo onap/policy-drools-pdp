457072459335f5fa8571b3cf5cd0286e68d858ab
Taka Cho <takamune.cho@att.com>
2020-06-24 16:24:26 -0400

    sonar fix - FeatureServerPool
    - sonar thinks one block of comments are code,
      so changed some syntax in comments
    - not more than one break or continue statement
    
    Issue-ID: POLICY-2616
    Change-Id: Ia35b73584f5a8492320c4beaa069ba7f903ce268
    Signed-off-by: Taka Cho <takamune.cho@att.com>

--- feature-server-pool/src/main/java/org/onap/policy/drools/serverpool/FeatureServerPool.java
+++ feature-server-pool/src/main/java/org/onap/policy/drools/serverpool/FeatureServerPool.java
@@ -120,8 +120,8 @@ public class FeatureServerPool
      * Example <field-list>: requestID,CommonHeader.RequestID
      *
      * Table generated from this example has length 2:
-     * table[0] = {"requestID"}
-     * table[1] = {"CommonHeader", "RequestID"}
+     * table 0 is "requestID"
+     * table 1 is "CommonHeader", "RequestID"
      */
     private static HashMap<String, String[][]> topicToPaths = new HashMap<>();
 
@@ -310,6 +310,7 @@ public class FeatureServerPool
              */
             String fieldName = path[path.length - 1];
             String conversionFunctionName = null;
+            String keywordLocal = null;
             int index = fieldName.indexOf(':');
 
             if (index > 0) {
@@ -319,17 +320,12 @@ public class FeatureServerPool
                 path[path.length - 1] = fieldName;
             }
             keyword = sco.getString(path);
-            if (keyword != null) {
-                if (conversionFunctionName == null) {
-                    // We found a keyword -- we don't need to try other paths,
-                    // so we should break out of the loop
-                    break;
-                }
 
-                // we have post-processing to do
-                keyword = Keyword.convertKeyword(keyword, conversionFunctionName);
-                if (keyword != null) {
-                    // conversion was successful
+            if (keyword != null) {
+                if (conversionFunctionName != null) {
+                    keywordLocal = Keyword.convertKeyword(keyword, conversionFunctionName);
+                }
+                if (conversionFunctionName == null || keywordLocal != null) {
                     break;
                 }
             }
