149df90bb72ece740accaf3ea037cf40defcd1bd
jhh <jorge.hernandez-herrero@att.com>
2021-03-09 10:01:17 -0600

    add no locking feature
    It provides an interface to applications for locking
    purposes that always succeeds.  It does not deny
    acquiring resource locks.
    
    Issue-ID: POLICY-3114
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: I6aebb5f40e4176e0ff7b0e6c6d4042a79dd0b7a3
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

---
+++ policy-core/src/main/java/org/onap/policy/drools/core/lock/AlwaysSuccessLock.java
@@ -0,0 +1,64 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.core.lock;
+
+import lombok.NonNull;
+
+/**
+ * Lock implementation whose operations always succeed.
+ */
+public class AlwaysSuccessLock extends LockImpl {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Overrides parent constructor.
+     */
+    public AlwaysSuccessLock() {
+        setState(LockState.ACTIVE);
+    }
+
+    /**
+     * Overrides parent constructor.
+     */
+    public AlwaysSuccessLock(@NonNull LockState state, @NonNull String resourceId,
+            @NonNull String ownerKey, int holdSec, @NonNull LockCallback callback) {
+        super(state, resourceId, ownerKey, holdSec, callback);
+        if (!isActive()) {
+            throw new IllegalArgumentException("AlwaysSuccessLock can only be created in the active state");
+        }
+    }
+
+    /**
+     * Constructs the object.
+     */
+    public AlwaysSuccessLock(@NonNull String resourceId, @NonNull String ownerKey,
+            int holdSec, @NonNull LockCallback callback) {
+        super(LockState.ACTIVE, resourceId, ownerKey, holdSec, callback);
+    }
+
+    /**
+     * Always returns true.
+     */
+    @Override
+    public synchronized boolean free() {
+        return true;
+    }
+}
