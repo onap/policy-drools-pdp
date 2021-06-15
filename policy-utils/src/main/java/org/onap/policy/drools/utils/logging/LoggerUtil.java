af4398c19298f049079a178fe7f7db96da8cda8b
jhh <jorge.hernandez-herrero@att.com>
2021-06-15 14:50:20 -0500

    filter logging input data per sonar security issue
    Issue-ID: POLICY-3289
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: Iad22a581a6bd98e7e210162d30ec7741972669d2

--- policy-utils/src/main/java/org/onap/policy/drools/utils/logging/LoggerUtil.java
+++ policy-utils/src/main/java/org/onap/policy/drools/utils/logging/LoggerUtil.java
@@ -86,7 +86,9 @@ public class LoggerUtil {
             throw new IllegalArgumentException("no logger " + loggerName);
         }
 
-        logger.setLevel(ch.qos.logback.classic.Level.toLevel(loggerLevel));
+        // use the current log level if the string provided cannot be converted to a valid Level.
+        logger.setLevel(ch.qos.logback.classic.Level.toLevel(loggerLevel, logger.getLevel()));
+
         return logger.getLevel().toString();
     }
 }
