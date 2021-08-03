d082f1598e514da15e0144f9b46ed14448bc39ca
Taka Cho <takamune.cho@att.com>
2021-08-03 13:28:28 -0400

    Use slf4j Marker from common repo
    using slf4j Marker from common repo
    
    Issue-ID: POLICY-3087
    Change-Id: Iefbd6fa9ab2dfa6388508cb6ffd6cf1358b45ccc
    Signed-off-by: Taka Cho <takamune.cho@att.com>

--- policy-utils/src/main/java/org/onap/policy/drools/utils/logging/LoggerUtil.java
+++ policy-utils/src/main/java/org/onap/policy/drools/utils/logging/LoggerUtil.java
@@ -20,16 +20,16 @@
 
 package org.onap.policy.drools.utils.logging;
 
-import ch.qos.logback.classic.LoggerContext;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
 /**
  * Loger Utils.
  */
-public class LoggerUtil {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class LoggerUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerUtil.class);
 
@@ -43,62 +43,4 @@ public class LoggerUtil {
      */
     public static final String LOGBACK_CONFIGURATION_FILE_DEFAULT = "config/logback.xml";
 
-    /**
-     * Root logger.
-     */
-    public static final String ROOT_LOGGER = "ROOT";
-
-    /**
-     * Metric Log Marker Name.
-     */
-    public static final String METRIC_LOG_MARKER_NAME = "metric";
-
-    /**
-     * Transaction Log Marker Name.
-     */
-    public static final String TRANSACTION_LOG_MARKER_NAME = "transaction";
-
-    /**
-     * Marks a logging record as a metric.
-     */
-    public static final Marker METRIC_LOG_MARKER = MarkerFactory.getMarker(METRIC_LOG_MARKER_NAME);
-
-    /**
-     * Marks a logging record as an end-to-end transaction.
-     */
-    public static final Marker TRANSACTION_LOG_MARKER = MarkerFactory.getMarker(TRANSACTION_LOG_MARKER_NAME);
-
-    private LoggerUtil() {
-        // Empty constructor
-    }
-
-    /**
-     * Set the log level of a logger.
-     *
-     * @param loggerName logger name
-     * @param loggerLevel logger level
-     */
-    public static String setLevel(String loggerName, String loggerLevel) {
-        if (!(LoggerFactory.getILoggerFactory() instanceof LoggerContext)) {
-            throw new IllegalStateException("The SLF4J logger factory is not configured for logback");
-        }
-
-        final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        final var logger = context.getLogger(loggerName);
-        if (logger == null) {
-            throw new IllegalArgumentException("no logger " + loggerName);
-        }
-
-        LOGGER.warn("setting {} logger to level {}", loggerName, loggerLevel);
-
-        // use the current log level if the string provided cannot be converted to a valid Level.
-
-        // NOSONAR: this method is currently used by the telemetry api (which should be authenticated).
-        // It is no more or no less dangerous than an admin changing the logback level on the fly.
-        // This is a controlled admin function that should not cause any risks when the system
-        // is configured properly.
-        logger.setLevel(ch.qos.logback.classic.Level.toLevel(loggerLevel, logger.getLevel()));  // NOSONAR
-
-        return logger.getLevel().toString();
-    }
 }
