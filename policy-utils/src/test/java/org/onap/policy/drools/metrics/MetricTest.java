a15310cb0b86ed488c0e0c3c9d8becbefcbac04f
danielhanrahan <daniel.hanrahan@est.tech>
2026-06-18 22:37:27 +0100

    Migrate pojo tests to centralized PojoTester utility
    - Simplify MetricTest to use common PojoTester API
    - Remove dead DroolsPolicyTest (tested immutable/builder class)
    - Remove direct openpojo dependency
    
    Issue-ID: POLICY-5592
    Change-Id: Ibbdb77be89a7c979b4b4ebe957237ddf734ace41
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>

--- policy-utils/src/test/java/org/onap/policy/drools/metrics/MetricTest.java
+++ policy-utils/src/test/java/org/onap/policy/drools/metrics/MetricTest.java
@@ -28,33 +28,18 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-import com.openpojo.reflection.PojoClass;
-import com.openpojo.reflection.impl.PojoClassFactory;
-import com.openpojo.validation.Validator;
-import com.openpojo.validation.ValidatorBuilder;
-import com.openpojo.validation.rule.impl.GetterMustExistRule;
-import com.openpojo.validation.rule.impl.SetterMustExistRule;
-import com.openpojo.validation.test.impl.GetterTester;
-import com.openpojo.validation.test.impl.SetterTester;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import org.junit.jupiter.api.Test;
+import org.onap.policy.common.utils.test.PojoTester;
 
 class MetricTest {
 
     @Test
     void testPojo() {
-        PojoClass metric = PojoClassFactory.getPojoClass(Metric.class);
-        Validator val = ValidatorBuilder
-            .create()
-            .with(new SetterMustExistRule())
-            .with(new GetterMustExistRule())
-            .with(new SetterTester())
-            .with(new GetterTester())
-            .build();
-        val.validate(metric);
+        PojoTester.testPojos(Metric.class.getPackageName());
     }
 
     @Test
